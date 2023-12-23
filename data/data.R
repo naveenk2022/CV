58450aef7950df6b1adaf337792e0ebb4ed51d69
naveenk2022 <naveenk@NaveenKannan>
2023-12-23 18:33:20 -0500

    Editing CV

--- data/data.R
+++ data/data.R
@@ -15,11 +15,13 @@ experience <- tribble(
   "Population Health and Quantitative Health Sciences, School of Medicine, Case Western Reserve University", 
   "May", 2023, 
   "Present", NA, "Cleveland, Ohio", 
-  "Docker/Singularity Containerization and Documentation. \\begin{itemize}
-        \\item Developed \\textbf{Docker/Singularity containers} for reproducible scientific pipelines, facilitating efficient workflows for colleagues.
+  "\\textbf{Docker/Singularity Containerization and Documentation.} \\begin{itemize}
+        \\item Developed Docker/Singularity containers for reproducible scientific pipelines, facilitating efficient workflows for colleagues.
         \\item Authored comprehensive documentation to ensure seamless adoption and easy troubleshooting.
-        \\item Incorporated Mamba into the scientific workflow, reducing container build times by \\textbf{300 percent}.\\end{itemize}
-  Cluster Configuration and Big Data Analytics \\begin{itemize}
-        \\item Configured and maintained both \\textbf{VM and Raspberry Pi bare-metal servers}.
-        \\item Implemented big data analytics tools \\textbf{(Hadoop, Spark)} along with bioinformatics tools like \\textbf{Hail and Glow} using Ansible scripts.\\end{itemize}"
+        \\item Incorporated Mamba into the scientific workflow, reducing container build times by 300 percent.\\end{itemize}
+  \\textbf{Cluster Configuration and Big Data Analytics} \\begin{itemize}
+        \\item Set up, configured and maintained both VM and Raspberry Pi bare-metal servers from scratch.
+        \\item Implemented big data analytics tools (Hadoop, Spark) along with bioinformatics tools like Hail and Glow using Ansible scripts.\\end{itemize}
+  \\textbf{Genomic data Analysis} \\begin{itemize}
+        \\item Collaborated with the Alzheimer's Disease Sequence Project by annotating and scoring the biological effects of 362 million structural genetic variants associated with Alzheimer's Disease.\\end{itemize}"
 )
