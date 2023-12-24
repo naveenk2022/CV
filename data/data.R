7acb31d1f78ff8c97c791e28e29f92b635cd6418
naveenk2022 <naveenk@NaveenKannan>
2023-12-23 21:21:38 -0500

    Edited Work Experience.

--- data/data.R
+++ data/data.R
@@ -23,9 +23,11 @@ experience <- tribble(
         \\item Incorporated Mamba into the scientific workflow, reducing container build times by 300 percent.\\end{itemize}
   \\textbf{Cluster Configuration and Big Data Analytics} \\begin{itemize}
         \\item Set up, configured and maintained both VM and Raspberry Pi bare-metal servers from scratch.
+        \\item Created in-depth documentation for the cluster set-up and configuration. 
         \\item Implemented big data analytics tools (Hadoop, Spark) along with bioinformatics tools like Hail and Glow using Ansible scripts.\\end{itemize}
   \\textbf{Genomic data Analysis} \\begin{itemize}
-        \\item Collaborated with the Alzheimer's Disease Sequence Project by annotating and scoring the biological effects of 362 million structural genetic variants associated with Alzheimer's Disease.\\end{itemize}",
+        \\item Collaborated with the Alzheimer's Disease Sequence Project by annotating and scoring the biological effects of 362 million structural genetic variants associated with Alzheimer's Disease.
+        \\item Performed statistical analysis on the data and presented findings in the form of a poster at the ASHG (American Society of Human Genetics) 2023 Conference in Washington DC.\\end{itemize}",
   "Graduate Research Assistant",
   "Population Health and Quantitative Health Sciences, School of Medicine, Case Western Reserve University",
   "JANUARY",2023,
@@ -38,8 +40,24 @@ experience <- tribble(
   "DECEMBER",2022,
   "Cleveland, Ohio",
   "\\textbf{Mapping human movement patterns after a natural disaster.} \\begin{itemize}
-        \\item Developed Python scripts automating GPS coordinate extraction from GPX files of geospatial video feeds in Democratic Republic of Congo post-volcano eruption.
+        \\item Developed Python scripts automating GPS coordinate extraction from GPX files of geospatial video feeds in Democratic Republic of Congo the volcanic eruption of Mt Nyiragongo.
         \\item Processed and transformed data into machine-readable format, enabling streamlined map generation.
         \\item Applied automated scripts to parse video feeds into frames, implementing Machine Learning Algorithms for tent detection.
-        \\item Wrote up the findings as a part of a manuscript for a peer-reviewed publication.\\end{itemize}"
+        \\item Created heat maps of tent distribution according to GPS coordinates, allowing for the mapping of the human movement pattern in the aftermath of the eruption. 
+        \\item Wrote up the findings as a part of a manuscript for a peer-reviewed publication.\\end{itemize}",
+  "Junior Resident Doctor",
+  "Department of Psychiatry, Saveetha Medical College",
+  "JULY", 2020,
+  "SEPTEMBER", 2020,
+  "Chennai, India",
+  "\\textbf{Diagnosing and managing patients with psychiatric illnesses} \\begin{itemize}
+        \\item Learned to evaluate and diagnose psychiatric illnesses in stressful environments.
+        \\item Assigned and achieved a goal of management of five patients per day.\\end{itemize}",
+  "Junior Resident Doctor",
+  "Madras Medical College",
+  "MARCH", 2019,
+  "MARCH", 2020,
+  "Chennai, India",
+  "\\textbf{Rotated through the following departments:} Internal Medicine, General Surgery, Obstetrics and Gynecology, Community Medicine, Psychiatry, Emergency Trauma Ward, Labor Ward."
+  
 )
